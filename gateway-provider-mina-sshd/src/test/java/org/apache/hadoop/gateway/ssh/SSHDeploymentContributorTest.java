bd2c2aaf911e7e9cefccf2ca5aa0c666ae9a26b7
Ed Kohlwey <ekohlwey@gmail.com>
2014-04-03 14:37:56 -0400

    KNOX-250 beginning work on an integration test.

--- gateway-provider-mina-sshd/src/test/java/org/apache/hadoop/gateway/ssh/SSHDeploymentContributorTest.java
+++ gateway-provider-mina-sshd/src/test/java/org/apache/hadoop/gateway/ssh/SSHDeploymentContributorTest.java
@@ -6,6 +6,12 @@ import org.apache.directory.server.annotations.CreateTransport;
 import org.apache.directory.server.core.annotations.ApplyLdifFiles;
 import org.apache.directory.server.core.integ.FrameworkRunner;
 import org.apache.directory.server.kerberos.kdc.AbstractKerberosITest;
+import org.apache.directory.server.kerberos.kdc.AbstractKnoxKerberosITest;
+import org.apache.directory.server.protocol.shared.transport.TcpTransport;
+import org.apache.directory.shared.kerberos.codec.types.EncryptionType;
+import org.apache.directory.shared.kerberos.crypto.checksum.ChecksumType;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -13,10 +19,16 @@ import org.junit.runner.RunWith;
 @CreateLdapServer(transports = { @CreateTransport(protocol = "LDAP") })
 @CreateKdcServer(transports = { @CreateTransport(protocol = "TCP", port = 6086) })
 @ApplyLdifFiles("org/apache/directory/server/kerberos/kdc/KerberosIT.ldif")
-public class SSHDeploymentContributorTest extends AbstractKerberosITest{
+public class SSHDeploymentContributorTest extends AbstractKnoxKerberosITest {
+
+  @Before
+  public void populateBasicKDC() throws Throwable {
+    setupEnv(new KnoxObtainTicketParameters(TcpTransport.class,
+        EncryptionType.DES3_CBC_SHA1, ChecksumType.HMAC_SHA1_DES3));
+  }
 
   @Test
-  public void testConnection() {
+  public void testConnection() throws Throwable {
 
   }
 
